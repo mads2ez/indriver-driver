93042b71c5e61947692b81b8ab37aaff6ed4654b
mads2ez <maximsivtsev@gmail.com>
2017-03-28 12:23:38 +0900

    1

--- js/my-app.js
+++ js/my-app.js
@@ -12,10 +12,6 @@ var mainView = myApp.addView('.view-main', {
 
 // Callbacks to run specific code for specific pages, for example for About page:
 myApp.onPageInit('payment', function (page) {
-    // run createContentPage func after link was clicked
-    $$('.create-page').on('click', function () {
-        createContentPage();
-    });
     $$('.card-popup-topup').hide();
     $$('.card-popup-activateshift').hide();
 });
@@ -31,7 +27,7 @@ $$('.c-bg').on('click', function (e) {
   $$('.card-popup-activateshift').hide();
 });
 
-myApp.onPageInit('index', function (page) {
+myApp.onPageAfterAnimation('index', function (page) {
   if (getAllUrlParams().paid === true) {
     $$('.card-popup-activateshift').show();
   }
@@ -42,7 +38,13 @@ $$('#showtoast').on('click', function(e) {
   $('.toast').fadeIn(400).delay(3000).fadeOut(400)
 });
 
+function openModal() {
+  if (getAllUrlParams(document.URL).paid === true) {
+    $$('.card-popup-activateshift').show();
+  }
+}
 
+openModal();
 
 function getAllUrlParams(url) {
 
@@ -106,59 +108,31 @@ function getAllUrlParams(url) {
   return obj;
 }
 
-// $('.tab-link').click(function() {
-//     $(this).closest( "img" ).attr('src', 'img/List-Selected.png');
-//     $('#tab-img__prior').attr('src', 'img/Priority.png');
-//     $('#tab-img__pay').attr('src', 'img/Pay.png');
-//     $('#tab-img__car').attr('src', 'img/Car.png');
-// });
-//
-// $('.tab-link').click(function() {
-//     $(this).closest('img').attr('src', 'img/Priority-Selected.png');
-//     $('#tab-img__list').attr('src', 'img/List.png');
-//     $('#tab-img__pay').attr('src', 'img/Pay.png');
-//     $('#tab-img__car').attr('src', 'img/Car.png');
-// });
-//
-// $('.tab-link').click(function() {
-//     $(this).closest( "img" ).attr('src', 'img/Pay-Selected.png');
-//     $('#tab-img__prior').attr('src', 'img/Priority.png');
-//     $('#tab-img__list').attr('src', 'img/List.png');
-//     $('#tab-img__car').attr('src', 'img/Car.png');
-// });
-//
-// $('.tab-link').click(function() {
-//     $(this).closest( "img" ).attr('src', 'img/Car-Selected.png');
-//     $('#tab-img__prior').attr('src', 'img/Priority.png');
-//     $('#tab-img__pay').attr('src', 'img/Pay.png');
-//     $('#tab-img__list').attr('src', 'img/List.png');
-// });
-
 // Generate dynamic page
-var dynamicPageIndex = 0;
-function createContentPage() {
-	mainView.router.loadContent(
-        '<!-- Top Navbar-->' +
-        '<div class="navbar">' +
-        '  <div class="navbar-inner">' +
-        '    <div class="left"><a href="#" class="back link"><i class="icon icon-back"></i><span>Back</span></a></div>' +
-        '    <div class="center sliding">Dynamic Page ' + (++dynamicPageIndex) + '</div>' +
-        '  </div>' +
-        '</div>' +
-        '<div class="pages">' +
-        '  <!-- Page, data-page contains page name-->' +
-        '  <div data-page="dynamic-pages" class="page">' +
-        '    <!-- Scrollable page content-->' +
-        '    <div class="page-content">' +
-        '      <div class="content-block">' +
-        '        <div class="content-block-inner">' +
-        '          <p>Here is a dynamic page created on ' + new Date() + ' !</p>' +
-        '          <p>Go <a href="#" class="back">back</a> or go to <a href="services.html">Services</a>.</p>' +
-        '        </div>' +
-        '      </div>' +
-        '    </div>' +
-        '  </div>' +
-        '</div>'
-    );
-	return;
-}
+// var dynamicPageIndex = 0;
+// function createContentPage() {
+// 	mainView.router.loadContent(
+//         '<!-- Top Navbar-->' +
+//         '<div class="navbar">' +
+//         '  <div class="navbar-inner">' +
+//         '    <div class="left"><a href="#" class="back link"><i class="icon icon-back"></i><span>Back</span></a></div>' +
+//         '    <div class="center sliding">Dynamic Page ' + (++dynamicPageIndex) + '</div>' +
+//         '  </div>' +
+//         '</div>' +
+//         '<div class="pages">' +
+//         '  <!-- Page, data-page contains page name-->' +
+//         '  <div data-page="dynamic-pages" class="page">' +
+//         '    <!-- Scrollable page content-->' +
+//         '    <div class="page-content">' +
+//         '      <div class="content-block">' +
+//         '        <div class="content-block-inner">' +
+//         '          <p>Here is a dynamic page created on ' + new Date() + ' !</p>' +
+//         '          <p>Go <a href="#" class="back">back</a> or go to <a href="services.html">Services</a>.</p>' +
+//         '        </div>' +
+//         '      </div>' +
+//         '    </div>' +
+//         '  </div>' +
+//         '</div>'
+//     );
+// 	return;
+// }
